41c44b120cf6896d208b81038284c14c7c7e7612
Myroslav13 <myroslavshchuruk@gmail.com>
2025-12-17 12:47:25 +0200

    Initial commit

---
+++ bookify/src/components/Login.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function Login() {
+
+  return (
+    <>
+      <form method="post" action="/login">
+
+      </form>
+    </>
+  )
+}
+
+export default Login
---
+++ bookify/src/components/Main.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function Main() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Main
